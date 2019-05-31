38a0074020dac1124afdfa7d8edcc0290658e47b
Tonio <pawcio577@gmail.com>
2019-05-31 04:15:14 -0700

    GM GS
    adding GameManager
    write new one GameScene [more modifications then standard one]

---
+++ Snake/GameManager.swift
@@ -0,0 +1,12 @@
+//
+//  GameManager.swift
+//  Snake
+//
+//  Created by tonio on 31/05/2019.
+//  Copyright © 2019 tonio. All rights reserved.
+//
+import SpriteKit
+import Foundation
+class GameManager {
+    
+}
